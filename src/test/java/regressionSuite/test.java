543fdd672a159c017f44104ed5b78353ed294cc8
antonissavvoglou <a.savvoglou@thebeat.co>
2018-09-14 11:55:31 +0300

    Added test class

---
+++ src/test/java/regressionSuite/test.java
@@ -0,0 +1,14 @@
+package regressionSuite;
+
+import org.testng.annotations.Test;
+
+public class test {
+
+    @Test
+    public void testing() {
+
+        System.out.println("The first test");
+
+    }
+
+}
